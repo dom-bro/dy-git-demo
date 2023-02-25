5712905b70cadce46dfd7eb7d1c3a3c24c0d2e3c
someone <516911028@qq.com>
2023-02-25 20:47:10 +0800

    zsh

---
+++ zsh.config.zsh
@@ -0,0 +1,12 @@
+alias s='git status'
+alias q='git pull'
+alias Q='git push'
+
+function mt () {
+  local source=$(current_branch)
+  git checkout $1
+  git pull
+  git merge $source
+  git push
+  git checkout $source
+}
